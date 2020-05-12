aeb70736b8a2348e57e6dd95416ce6cb3737b025
Faisal Hossain Shuvo <shuvo586@gmail.com>
2020-05-13 03:25:33 +0600

    Added Customer Profile Active Cart tab

--- includes/classes/customer/class-sxp-customer-profile-table.php
+++ includes/classes/customer/class-sxp-customer-profile-table.php
@@ -345,7 +345,95 @@ class SXP_Customer_Profile_Table {
 						</table>
 					</div><!-- end .tab-content -->
 					<div id="active-cart" class="tab-content">
-						Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						<table class="sxp-table sxp-customer-profile-table sxp-active-cart-table">
+							<thead>
+								<tr>
+									<th scope="col" class="manage-column column-title column-primary sortable desc"><a href="#">Sl.</a></th>
+									<th scope="col" class="manage-column column-title column-primary sortable desc"><a href="#">All Products</a></th>
+									<th scope="col" class="manage-column column-title column-primary sortable desc"><a href="#">Unit Price</a></th>
+									<th scope="col" class="manage-column column-title column-primary sortable desc"><a href="#">QTY</a></th>
+									<th scope="col" class="manage-column column-title column-primary sortable desc"><a href="#">Amount</a></th>
+								</tr>
+							</thead>
+							<tbody id="the-list">
+								<tr>
+									<td>1.</td>
+									<td data-colname="All Products">
+										<div class="product-thumb"><img src="https://via.placeholder.com/40" alt="Product"></div>
+										<div class="product-details">
+											<div class="product-name">Premium Miniket Rice</div>
+											<div class="product-sku">OC063-0001</div>
+										</div>
+									</td>
+									<td data-colname="Unit Price">$61.87</td>
+									<td data-colname="QTY">x 2</td>
+									<td data-colname="Amount">$123.74</td>
+								</tr>
+								<tr>
+									<td>2.</td>
+									<td data-colname="All Products">
+										<div class="product-thumb"><img src="https://via.placeholder.com/40" alt="Product"></div>
+										<div class="product-details">
+											<div class="product-name">Premium Miniket Rice</div>
+											<div class="product-sku">OC063-0001</div>
+										</div>
+									</td>
+									<td data-colname="Unit Price">$61.87</td>
+									<td data-colname="QTY">x 2</td>
+									<td data-colname="Amount">$123.74</td>
+								</tr>
+								<tr>
+									<td>3.</td>
+									<td data-colname="All Products">
+										<div class="product-thumb"><img src="https://via.placeholder.com/40" alt="Product"></div>
+										<div class="product-details">
+											<div class="product-name">Premium Miniket Rice</div>
+											<div class="product-sku">OC063-0001</div>
+										</div>
+									</td>
+									<td data-colname="Unit Price">$61.87</td>
+									<td data-colname="QTY">x 2</td>
+									<td data-colname="Amount">$123.74</td>
+								</tr>
+							</tbody>
+							<tfoot>
+								<tr>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td>Sub Total</td>
+									<td>$5,641.01</td>
+								</tr>
+								<tr>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td>Tax Included</td>
+									<td>+$550.00</td>
+								</tr>
+								<tr>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td>Shipping Charge</td>
+									<td>+$250.00</td>
+								</tr>
+								<tr>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td>Discount Received</td>
+									<td>-$1,500.00</td>
+								</tr>
+								<tr>
+									<td><i data-feather="file-text"></i> Place Order</td>
+									<td><i data-feather="mail"></i> Send Mail</td>
+									<td></td>
+									<td>Grand Total</td>
+									<td>$4,941.01‬</td>
+								</tr>
+							</tfoot>
+						</table>
 					</div><!-- end .tab-content -->
 					<div id="searches" class="tab-content">
 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
