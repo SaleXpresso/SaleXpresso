c8882a222f659f0b4aa322a1cd2b4a10aef2a384
Faisal Hossain Shuvo <shuvo586@gmail.com>
2020-06-04 08:52:34 +0600

    added datepicker styles

--- src/js/admin.js
+++ src/js/admin.js
@@ -46,7 +46,10 @@ import './components/_accordion';
 				'Last 30 Days': [ moment().subtract( 29, 'days' ), moment() ],
 				'This Month': [ moment().startOf( 'month' ), moment().endOf( 'month' ) ],
 				'Last Month': [ moment().subtract( 1, 'month' ).startOf( 'month' ), moment().subtract( 1, 'month' ).endOf( 'month' ) ],
+				'This Year': [ moment().startOf( 'year' ), moment().endOf( 'year' ) ],
+				'Last Year': [ moment().subtract( 1, 'year' ).startOf( 'year' ), moment().subtract( 1, 'year' ).endOf( 'year' ) ],
 			},
+			autoApply: true,
 		}, cb );
 
 		cb( start, end );
